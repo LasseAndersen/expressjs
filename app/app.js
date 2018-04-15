80e518933394015b0570fe347a6bbe70c3f06754
Lasse Andersen <LasseAndersen@users.noreply.github.com>
2018-04-15 21:50:52 +0200

    Handling HTTP requests with express

---
+++ app/app.js
@@ -0,0 +1,14 @@
+// var http = require('http');
+// var myServer = http.createServer(function(request, response)
+// {
+// response.writeHead(200, {"content-type" : "text/html"});
+
+// response.write('<h1>roux meetups</h1>');
+
+// response.end();
+// });
+
+// myServer.listen(3000);
+
+// console.log(' go to http://localhost:300 on your server')
+
