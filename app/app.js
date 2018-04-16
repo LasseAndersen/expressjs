07dbe7255dd4c8efbe6e1945ddbb28c86684e582
Lasse Andersen <LasseAndersen@users.noreply.github.com>
2018-04-16 12:25:24 +0200

    bla
    bla

--- app/app.js
+++ app/app.js
@@ -5,20 +5,38 @@ var dataFile = require('./data/data.json');
 app.set('port', process.env.PORT || 3000 );
 
 app.get('/', function(req, res) {
-  var info = '';
-  dataFile.speakers.forEach(function(item) {
-    info += `
-    <li>
-      <h2>${item.name}</h2>
-      <p>${item.summary}</p>
-    </li>
-    `;
-  });
   res.send(`
-      <h1>Roux Academy Meetups</h1>
-      ${info}
+      <h1>welcome</h1>
+      <p>Roux Academy Meetups put together artists from all walk of life</p>
   `);
 });
+
+
+app.get('/speakers', function(req, res) {
+    var info = '';
+    dataFile.speakers.forEach(function(item) {
+      info += `
+      <li>
+        <h2>${item.name}</h2>
+        <p>${item.summary}</p>
+      </li>
+      `;
+    });
+    res.send(`
+        <h1>Roux Academy Meetups</h1>
+        ${info}
+    `);
+  });
+
+  app.get('/speakers/:speakerid', function(req, res) {
+    var speaker = dataFile.speakers[req.params.speakerid];
+    
+    res.send(`
+        <h1>${speaker.title}</h1>
+        <h2>${speaker.name}</h2>
+        <p>${speaker.summary}</p>
+    `);
+  });
 
 var server = app.listen(app.get('port'), function() {
   console.log('Listening on port ' + app.get('port'));
